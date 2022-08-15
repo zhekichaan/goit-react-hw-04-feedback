e0cffcb34578e66340f480abb47cbb659a982b3b
zhekichaan <eugene.chernitskiy@gmail.com>
2022-08-15 19:48:34 -0400

    1

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -4,7 +4,6 @@ import 'modern-normalize';
 export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
-    background-color: #ECECEC;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
